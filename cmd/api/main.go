eff3e0630d09031a058c6aca4f40bc75fc3216c3
Azul Gautam <azulgautam79@gmail.com>
2026-09-10 21:22:00 +0545

    refactor: main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/azulgautam79/olx-api/internal/config"
+	"github.com/azulgautam79/olx-api/internal/handlers"
 )
 
 func main() {
@@ -14,12 +15,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-
-		w.Write([]byte(`{"status":"ok"}`))
-	})
+	mux.HandleFunc("GET /healthz", handlers.Healthz)
 
 	srv := http.Server{
 		Addr:         ":" + cfg.Port,
