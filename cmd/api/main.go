d64457e4a5777762eda9c76c0a52e1e609f267f5
Azul Gautam <azulgautam79@gmail.com>
2026-09-10 20:35:19 +0545

    feat: config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,13 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/azulgautam79/olx-api/internal/config"
 )
 
 func main() {
+
+	cfg := config.MustLoad()
 
 	mux := http.NewServeMux()
 
@@ -18,12 +22,14 @@ func main() {
 	})
 
 	srv := http.Server{
-		Addr:         ":8090",
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
+
+	log.Printf("Server is listening on %s", srv.Addr)
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Server failed: %v", err)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/azulgautam79/olx-api
 
 go 1.26.5
+
+require github.com/joho/godotenv v1.5.1
