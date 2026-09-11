3c328e1cf38947aa1ed28d740dfa4cfff11a8bd9
Azul Gautam <azulgautam79@gmail.com>
2026-09-11 18:25:59 +0545

    refactor: constructor pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,11 +23,13 @@ func main() {
 	fmt.Println("database connected")
 	fmt.Println("starting olx server...")
 
+	lh := handlers.NewListingHandler(db)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", handlers.Healthz)
-	mux.HandleFunc("GET /listings", handlers.Listings(db))
-	mux.HandleFunc("DELETE /listings/{id}", handlers.DeleteListing(db))
+	mux.HandleFunc("GET /listings", lh.GetLists)
+	mux.HandleFunc("DELETE /listings/{id}", lh.Delete)
 
 	srv := http.Server{
 		Addr:         ":" + cfg.Port,
